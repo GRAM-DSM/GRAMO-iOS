089b0a176b0f599bc2b1679fd310d6e218ad3f01
jangseoyoung <jangseoyoung0402@gmail.com>
2021-03-20 23:28:03 +0900

    [CREATE] GetSubmitted HomeworkList

---
+++ GRAMO/GRAMO/Model/homeworkModel/NetworkingModel/GetSubmitted HomeworkList.swift
@@ -0,0 +1,12 @@
+//
+//  GetSubmitted HomeworkList.swift
+//  GRAMO
+//
+//  Created by 장서영 on 2021/03/19.
+//
+
+import Foundation
+
+struct HomeworkListResponseS {
+    var homeworklistS = [HwStudent]()
+}

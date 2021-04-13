e4c1aa9f6537e859ea56e3139d1e05d772a54b6b
jangseoyoung <jangseoyoung0402@gmail.com>
2021-04-13 20:12:52 +0900

    Update : infoListVC

--- GRAMO/GRAMO/VC/info/infoListVC.swift
+++ GRAMO/GRAMO/VC/info/infoListVC.swift
@@ -8,26 +8,32 @@
 import UIKit
 
 var infoList = [InfoList]()
+var nationalTableView : UITableView!
 
 class infoListVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
     
-
+    let httpClient = HTTPClient()
+    private var getListModel: [Notice] = [Notice]()
+    private var listModel : GetNoticeList = GetNoticeList(notice: [Notice(id: 1, title: "asdf", content: "Asdf", user_email: "Asdf", created_at: "Asdf")])
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return infoList.count
+        return listModel.notice.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "list cell", for: indexPath) as! infoTableViewCell
         
         let date = DateFormatter()
-        date.dateFormat = "yyyy년 MM월 dd일"
+        date .dateFormat = "yyyy년 MM월 dd일"
         let currentDate = date.string(from: Date())
         cell.dateLabel.text = currentDate
         
-        cell.infoTitleLabel?.text = infoList[indexPath.row].infotTitle
-        cell.infoDetailLabel?.text = infoList[indexPath.row].infoDetail
+        cell.infoTitleLabel?.text = listModel.notice[indexPath.row].title
+        cell.infoDetailLabel?.text = listModel.notice[indexPath.row].content
+        cell.dateLabel?.text = listModel.notice[indexPath.row].created_at
+        //cell.writersName?.text = listModel.notice[indexPath.row].name
         
         return cell
     }
@@ -37,9 +43,10 @@ class infoListVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
-        tableView.rowHeight = 120
-
-       
+        tableView.rowHeight = 130
+        
+        getList()
+        nationalTableView = tableView
         
         // Do any additional setup after loading the view.
     }
@@ -48,7 +55,30 @@ class infoListVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
         tableView.reloadData()
     }
     
-
+    func getList(){
+        print("호출됨")
+        httpClient.get(NetworkingAPI.getNoticeList(1, 100)).responseJSON{(response) in
+            switch response.response?.statusCode{
+            case 200: print("OK - Send notice list successfully.")
+                do{
+                    print(response.data)
+                    guard let data = response.data else {return}
+                    guard let model = try? JSONDecoder().decode(GetNoticeList.self, from: data) else {return}
+                    print(model.notice)
+                    self.listModel.notice.append(contentsOf: model.notice)
+                    self.tableView.reloadData()
+                }
+                catch{
+                    print("error")
+                    print(error)
+                }
+            case 404: print("404 : NOT FOUND - Notice does not exist.")
+            default: print(response.response?.statusCode)
+            }
+        }
+    }
+    
+    
     /*
     // MARK: - Navigation
 

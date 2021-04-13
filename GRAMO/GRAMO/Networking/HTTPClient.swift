1c4dd1f9b7fbb5a1e8b4c1962e41719b7cdc3de5
jangseoyoung <jangseoyoung0402@gmail.com>
2021-04-13 20:12:36 +0900

    Update : Networking

--- GRAMO/GRAMO/Networking/HTTPClient.swift
+++ GRAMO/GRAMO/Networking/HTTPClient.swift
@@ -8,4 +8,31 @@
 import Foundation
 import Alamofire
 
+protocol HTTPClientProvider {
+    func get(_ api: NetworkingAPI) -> DataRequest
+    func post(_ api: NetworkingAPI) -> DataRequest
+    func patch(_ api: NetworkingAPI) -> DataRequest
+    func delete(_ api: NetworkingAPI) -> DataRequest
+}
+
+class HTTPClient : HTTPClientProvider {
+    
+    let baseURI = String()
+
+    func get(_ api: NetworkingAPI) -> DataRequest {
+        return AF.request(baseURI + api.path, method: .get, parameters: api.parameter, encoding: URLEncoding.default, headers: api.header, interceptor: nil)
+    }
+    
+    func post(_ api: NetworkingAPI) -> DataRequest {
+        return AF.request(baseURI + api.path, method: .post, parameters: api.parameter, encoding: JSONEncoding.prettyPrinted, headers: api.header, interceptor: nil)
+    }
+    
+    func patch(_ api: NetworkingAPI) -> DataRequest {
+        return AF.request(baseURI + api.path, method: .patch, parameters: api.parameter, encoding: JSONEncoding.prettyPrinted, headers: api.header, interceptor: nil)
+    }
+    
+    func delete(_ api: NetworkingAPI) -> DataRequest {
+        return AF.request(baseURI + api.path, method: .delete, parameters: api.parameter, encoding: JSONEncoding.prettyPrinted, headers: api.header, interceptor: nil)
+    }
+}
 
--- GRAMO/GRAMO/Networking/Networking.swift
+++ GRAMO/GRAMO/Networking/Networking.swift
@@ -8,7 +8,93 @@
 import Foundation
 import Alamofire
 
+let baseURINotice = "http://13.209.8.210:5000/"
+let baseURIHw = "http://211.38.86.92:8001"
+
+
 enum NetworkingAPI {
-    case getNoticeList(_ size: Int)
+    case getNoticeList(_ off_set : Int, _ limit_num : Int)
+    case getNoticeDetail(_ id: Int)
+    case createNotice(_ title: String, _ content: String)
+    case deleteNotice(_ id: Int)
     
+    case getAssHwList
+    case getSubHwList
+    case getOrdHwList
+    
+    case getHwContent(_ homeworkId: Int)
+    case createHw(_ major: String, _ endDate: String, _ studentEmail: String, _ description: String, _ title: String)
+    case deleteHw(_ detailId: Int)
+    
+    case submitHw(_ homeworkId: Int)
+    case rejectHw(_ homeworkId: Int)
+    
+    case getUserList
+    
+    
+    var path: String {
+        switch self {
+        case .getNoticeList(let off_set, let limit_num):
+            return baseURINotice + "/notice/\(off_set)/\(limit_num)"
+        case .createNotice:
+            return baseURINotice + "/notice"
+        case .getNoticeDetail(let id), .deleteNotice(let id):
+            return baseURINotice + "/notice/\(id)"
+        case .getAssHwList:
+            return baseURIHw + "/homework/assign"
+        case .getSubHwList:
+            return baseURIHw + "/homework/submit"
+        case .getOrdHwList:
+            return baseURIHw + "/homework/order"
+        case .getHwContent(let homeworkId):
+            return baseURIHw + "/homework/\(homeworkId)"
+        case .createHw:
+            return baseURIHw + "/homework/detail"
+        case .deleteHw(let detailId):
+            return baseURIHw + "/homework"
+        case .submitHw(let homeworkId):
+            return baseURIHw + "/homework/\(homeworkId)"
+        case .rejectHw(let homeworkId):
+            return baseURIHw + "/homework/reject/\(homeworkId)"
+        case .getUserList:
+            return baseURIHw + "/user/list"
+        
+        }
+    }
+    
+    var header: HTTPHeaders? {
+        switch self {
+        default:
+            return ["Authorization" : "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTYxODMwMDMxMCwianRpIjoiMzFmZmY0MjItYzQ4OS00ZDk1LWEwN2QtYjBlMmUxZTc4ODEwIiwibmJmIjoxNjE4MzAwMzEwLCJ0eXBlIjoiYWNjZXNzIiwic3ViIjoidGVzdGlvc0BnbWFpbC5jb20iLCJleHAiOjE2MTgzODY3MTB9.8mcy-vd7vCAVL39a0fOrx5ax4F1CAX7JfZGr5OyDxEU"]
+        }
+        
+//        let accessToken : String = "access_token"
+//                    let userDefault = UserDefaults.standard
+//                    userDefault.set(accessToken, forKey: "access_token")
+//                    userDefault.synchronize( )
+//                    guard let token = userDefault.string(forKey: "access_token") else { return nil }
+//                    return ["Authorization" : "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTYxODMwMDMxMCwianRpIjoiMzFmZmY0MjItYzQ4OS00ZDk1LWEwN2QtYjBlMmUxZTc4ODEwIiwibmJmIjoxNjE4MzAwMzEwLCJ0eXBlIjoiYWNjZXNzIiwic3ViIjoidGVzdGlvc0BnbWFpbC5jb20iLCJleHAiOjE2MTgzODY3MTB9.8mcy-vd7vCAVL39a0fOrx5ax4F1CAX7JfZGr5OyDxEU"]
+    }
+    
+    var parameter: [String:Any] {
+        switch self {
+//        case .getNoticeList(let off_set, let limit_num):
+            
+        case .getNoticeDetail(let id):
+            return ["id":id]
+        case .createNotice(let title, let content):
+            return ["title":title, "content":content]
+        case .getHwContent(let homeworkId):
+            return ["homeworkId":homeworkId]
+        case .createHw(let major, let endDate, let studentEmail, let description, let title):
+            return ["major":major, "endDate":endDate, "studentEmail":studentEmail, "description":description, "title":title]
+        case .deleteHw(let deleteId):
+            return ["deleteId":deleteId]
+        case .submitHw(let homeworkId):
+            return ["homeworkId":homeworkId]
+        case .rejectHw(let homeworkId):
+            return ["homeworkId":homeworkId]
+        default : return [:]
+        }
+    }
 }

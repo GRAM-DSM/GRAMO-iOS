73a894d61b291c5c84c331f92e5800133b3d8d25
jangseoyoung <jangseoyoung0402@gmail.com>
2021-04-22 22:16:07 +0900

    Update : homeworkAdd

--- GRAMO/GRAMO/VC/homework/homeworkAddVC.swift
+++ GRAMO/GRAMO/VC/homework/homeworkAddVC.swift
@@ -13,20 +13,27 @@ class homeworkAddVC: UIViewController, UITextViewDelegate {
     
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var selectMajorButton: UIButton!
-    @IBOutlet weak var deadLineButton: UIButton!
+    @IBOutlet weak var deadLinetTxt: UITextField!
     @IBOutlet weak var allocatorButton: UIButton!
     @IBOutlet weak var titleTxt: UITextField!
     @IBOutlet weak var detailTxtView: UITextView!
     
+    let datePicker = UIDatePicker()
+    
     let httpclient = HTTPClient()
     var majorItem = String()
     var allocatorItem = String()
+    var requestDate = String()
+    var studentEmail = String()
+    var requestMajor = String()
     
     private var userModel : [UserList] = [UserList]()
     private var userListModel : User = User()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        createDatePicker()
+        
         if dateLabel.adjustsFontSizeToFitWidth == false {
             dateLabel.adjustsFontSizeToFitWidth = true
         }
@@ -43,25 +50,31 @@ class homeworkAddVC: UIViewController, UITextViewDelegate {
     }
     
     @IBAction func addButton(_ sender: UIBarButtonItem) {
-        
+        addHw()
     }
     
     @IBAction func selectMajor(_ sender: UIButton){
-        
-        
-        
         let dropdown = DropDown()
         dropdown.dataSource = ["iOS", "안드로이드", "서버", "디자인"]
         dropdown.show()
         dropdown.selectionAction = { [unowned self] (index: Int, item: String) in
             selectMajorButton.setTitle("\(item)", for: .normal)
             majorItem = item
+            
+            switch majorItem {
+            case "iOS": requestMajor = "IOS"
+            case "안드로이드": requestMajor = "ANDROID"
+            case "서버" : requestMajor = "BACKEND"
+            case "디자인" : requestMajor = "DESIGN"
+            default:
+                print("TTI YONG")
+            }
         }
         //dropdown.bottomOffset = CGPoint(x: 0, y:(dropdown.anchorView?.plainView.bounds.height)!)
     }
     
     @IBAction func selectAllocator(_ sender: UIButton){
-        httpclient.get(NetworkingAPI.getUserList).responseJSON {(res) in
+        httpclient.get(NetworkingAPI.getUserList).responseJSON { [self](res) in
             print(res.data)
             switch res.response?.statusCode{
             case 200 :
@@ -84,7 +97,7 @@ class homeworkAddVC: UIViewController, UITextViewDelegate {
                     dropDown.show()
                     dropDown.selectionAction = {[unowned self] (index: Int, item: String) in
                         allocatorButton.setTitle("\(item)", for: .normal)
-                        allocatorItem = item
+                        self.studentEmail = model.userInfoResponses[index].email
                     }
                     
                 }
@@ -119,11 +132,59 @@ class homeworkAddVC: UIViewController, UITextViewDelegate {
         }
     }
     
+    func createDatePicker() {
+        
+        deadLinetTxt.textAlignment = .center
+        
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+        toolBar.setItems([doneButton], animated: true)
+        
+        deadLinetTxt.inputAccessoryView = toolBar
+        
+        deadLinetTxt.inputView = datePicker
+        
+        datePicker.preferredDatePickerStyle = .wheels
+        
+        datePicker.datePickerMode = .date
+        
+        datePicker.locale = Locale(identifier: "ko-KR")
+    }
+    
+    @objc func donePressed() {
+        let formatter = DateFormatter()
+        
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        
+        formatter.dateFormat = "yyyy년 MM월 dd일"
+        deadLinetTxt.text = formatter.string(from: datePicker.date)
+        
+        let requestFormatter = DateFormatter()
+        requestFormatter.dateFormat = "yyyy-MM-dd"
+        
+        requestDate = requestFormatter.string(from: datePicker.date)
+        
+        print(requestDate)
+        
+        self.view.endEditing(true)
+    }
+    
     func addHw() {
         print("addHw")
-        httpclient.post(NetworkingAPI.createHw(majorItem, deadLinetTxt.text!, allocatorItem, detailTxtView.text!, titleTxt.text!)).responseJSON{(res) in
+        print(studentEmail)
+        httpclient.post(NetworkingAPI.createHw(requestMajor, requestDate, studentEmail, detailTxtView.text!, titleTxt.text!)).responseJSON{(res) in
             switch res.response?.statusCode{
-            case 201 : print("OK - CREATED")
+            case 201 :
+                do{
+                    print("OK - CREATED")
+                    self.navigationController?.popViewController(animated: true)
+                }
+                catch {
+                    print("error: \(error)")
+                }
             case 400 : print("400 - BAD REQUEST")
             case 404 : print("404 - NOT FOUND")
             default : print(res.response?.statusCode)

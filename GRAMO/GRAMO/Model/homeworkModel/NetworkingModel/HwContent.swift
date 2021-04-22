e0804c359595d21ac1a51eda1215476152289f23
jangseoyoung <jangseoyoung0402@gmail.com>
2021-04-22 22:16:45 +0900

    Update : ListContentModel

--- GRAMO/GRAMO/Model/homeworkModel/NetworkingModel/HwContent.swift
+++ GRAMO/GRAMO/Model/homeworkModel/NetworkingModel/HwContent.swift
@@ -8,23 +8,24 @@
 import Foundation
 
 struct HwContent: Codable {
+    var homeworkId = Int()
     var title = String()
     var description = String()
     var startDate = String()
     var endDate = String()
-    var isSubmitted = String()
-    var isMine = String()
+    var isRejected = Bool()
     var major = String()
     var teacherName = String()
+    var studentName = String()
     
-    init(title: String, description: String, startDate: String, endDate: String,
-         isSubmitted: String, isMine: String, major: String, teacherName: String) {
+    init(homeworkId: Int, title: String, description: String, startDate: String, endDate: String,
+         isRejected: Bool, major: String, teacherName: String, studentName: String) {
+        self.homeworkId = homeworkId
         self.title = title
         self.description = description
         self.startDate = startDate
         self.endDate = endDate
-        self.isSubmitted = isSubmitted
-        self.isMine = isMine
+        self.isRejected = isRejected
         self.major = major
         self.teacherName = teacherName
     }

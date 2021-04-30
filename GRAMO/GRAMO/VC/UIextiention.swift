700355505a36221d9e238a87a7c68da2e27dacd4
jangseoyoung <jangseoyoung0402@gmail.com>
2021-04-30 16:03:42 +0900

    Move : UIextention

--- GRAMO/GRAMO/VC/UIextiention.swift
+++ GRAMO/GRAMO/VC/UIextiention.swift
@@ -40,5 +40,12 @@ extension UIViewController {
     }
     
     
+    func setNavigationBar(){
+        let bar:UINavigationBar! =  self.navigationController?.navigationBar
+        bar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        bar.shadowImage = UIImage()
+        bar.backgroundColor = UIColor.clear
+    }
+    
     
 }

cdf01f733f9af29b61e15057062899c3c144a7d1
jangseoyoung <jangseoyoung0402@gmail.com>
2021-04-30 16:06:57 +0900

    Update : SideMenu

--- GRAMO/GRAMO/VC/SideMenu/SideMenuNavigationController.swift
+++ GRAMO/GRAMO/VC/SideMenu/SideMenuNavigationController.swift
@@ -9,10 +9,10 @@ import UIKit
 import SideMenu
 
 class SideMenuNav: SideMenuNavigationController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         self.presentationStyle = .menuSlideIn
         self.leftSide = true
         self.menuWidth = self.view.frame.width * 0.45
@@ -21,15 +21,15 @@ class SideMenuNav: SideMenuNavigationController {
     
     
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }

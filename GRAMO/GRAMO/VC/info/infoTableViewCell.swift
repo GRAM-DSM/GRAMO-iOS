d3efc1c8badeb4c931c374c323e2b0e0f00065c2
jangseoyoung <jangseoyoung0402@gmail.com>
2021-04-30 16:07:18 +0900

    Update : tableViewCell UI

--- GRAMO/GRAMO/VC/info/infoTableViewCell.swift
+++ GRAMO/GRAMO/VC/info/infoTableViewCell.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class infoTableViewCell: UITableViewCell {
+class InfoTableViewCell: UITableViewCell {
     
     @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var writersName: UILabel!

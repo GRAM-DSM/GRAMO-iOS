eb65e51c74683328bc149d054b99ac26684e81bf
JeongChangYong <ckddyd1202@daum.net>
2021-05-07 16:25:22 +0900

    Update : API

---
+++ GRAMO/GRAMO/API/SignIn.swift
@@ -0,0 +1,22 @@
+//
+//  SignIn.swift
+//  GRAMO
+//
+//  Created by 정창용 on 2021/04/29.
+//
+
+import Foundation
+
+struct SignIn: Codable {
+    var name = String()
+    var major = String()
+    var access_token = String()
+    var refresh_token = String()
+    
+    init (name: String, major: String, access_token: String, refresh_token: String) {
+        self.name = name
+        self.major = major
+        self.access_token = access_token
+        self.refresh_token = refresh_token
+    }
+}
